4f88c9693e1fda561ec53221abb320dd19dc6bbf
256paul-mp <pmakumbi@byupathway.edu>
2025-03-09 19:31:09 +0300

    finished exercise 5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -11,30 +11,36 @@ class Program
         int usernumber = 0;
         while (usernumber != -1)
         {
-            Console.Write("Enter a number: ");
-                //computing for the sum of the numbers
-                int sum = 0;
-                foreach (int number in numbers)
-                {
-                    sum += number;
-
-                }
-                Console.WriteLine($"The sum of the numbers is {sum}");
-                //computing for the average of the numbers
-                float average = sum / numbers.Count;
-                Console.WriteLine($"The average of the numbers is {average}");
-                //computing for the maximum number
-                int max = numbers[0];
-
-                foreach (int number in numbers)
-                {
-                    if (number > max)
-                    {
-                        max = number;
-                    }
-                }
-                Console.WriteLine($"The maximum number is {max}");
+            Console.Write("Enter a number (-1 to quit): ");
+            usernumber = int.Parse(Console.ReadLine());
+            if (usernumber != -1)
+            {
+                numbers.Add(usernumber);
             }
         }
+
+        //computing for the sum of the numbers
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is {sum}");
+
+        //computing for the average of the numbers
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average of the numbers is {average}");
+
+        //computing for the maximum number
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is {max}");
+        }
     }
-}
+
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+        string username = promptUsername();
+        int usernumber = promptUserNumber();
+
+        int squarednumber = SquaredNumber(usernumber);
+
+        DisplayResult(username, squarednumber);
+        static int SquaredNumber(int number)
+        {
+            return number * number;
+        }
+    
+        static string promptUsername()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
+        static int promptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program");
     }
 }
