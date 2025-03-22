e2dde6bf68d5276ad70ef9d9ba7a8327500b028b
256paul-mp <pmakumbi@byupathway.edu>
2025-03-22 18:44:32 +0300

    Implement Fraction class with constructors and methods for string representation and decimal value

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,23 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction(1, 2);
+        Console.WriteLine(f1.getFractionString());
+        Console.WriteLine(f1.getDecimalValue());
+
+        Fraction f2 = new Fraction(3);
+        Console.WriteLine(f2.getFractionString());
+        Console.WriteLine(f2.getDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(3, 4);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
